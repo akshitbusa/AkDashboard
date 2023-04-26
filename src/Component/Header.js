1ba20a3e3116440c83ec7b92c04ddff153949698
akshitbusa <akshitbusa2002@gmail.com>
2023-04-26 22:44:12 +0530

    profile

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,5 +1,5 @@
 import logo from '../Photos/logo.png'
-import profile from '../Photos/profile-img.jpg'
+import profile from '../Photos/WhatsApp Image 2023-04-26 at 10.31.57 PM.jpeg'
 import message from '../Photos/messages-1.jpg'
 import message2 from '../Photos/messages-2.jpg'
 import message3 from '../Photos/messages-3.jpg'
@@ -210,15 +210,15 @@ const Header = () => {
                         <li className="nav-item dropdown pe-3">
 
                             <a className="nav-link nav-profile d-flex align-items-center pe-0" href="/" data-bs-toggle="dropdown">
-                                <img src={profile} alt="Profile" className="rounded-circle" />
-                                <span className="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+                                {/* <img src={profile} alt="Profile" className="rounded-circle" /> */}
+                                <span className="d-none d-md-block dropdown-toggle ps-2">Akshit Busa</span>
                             </a>
                             {/* <!-- End Profile Image Icon --> */}
 
                             <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
                                 <li className="dropdown-header">
-                                    <h6>Kevin Anderson</h6>
-                                    <span>Web Designer</span>
+                                    <h6>Akshit Busa</h6>
+                                    <span>Web devlopment</span>
                                 </li>
                                 <li>
                                     <hr className="dropdown-divider" />
