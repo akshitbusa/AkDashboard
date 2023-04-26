c2ad44d8b0c65d9661dd8d8a13d3f064098c36d7
akshitbusa <akshitbusa2002@gmail.com>
2023-04-26 22:19:45 +0530

    login

--- src/Component/Login.js
+++ src/Component/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
                                     <div className="d-flex justify-content-center py-4">
                                         <a href="index.html" className="logo d-flex align-items-center w-auto">
-                                            <img src={logo} alt="" />
+                                            <img src={logo} alt="img logo" />
                                             <span className="d-none d-lg-block">NiceAdmin</span>
                                         </a>
                                     </div>
